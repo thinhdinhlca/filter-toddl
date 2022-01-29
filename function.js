e50537e3120749044f1e44aff14b5c88c003ecec
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2022-01-29 12:12:54 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,18 +1,24 @@
-window.function = function (location, gender, price, htype) {
+window.function = function (age, city, location, interest, language, week, all) {
 
 //var arrays = [['apple', 'orange', 'banana', 'pear', 'fish', 'pancake', 'taco', 'pizza'],['taco', 'fish', 'apple', 'pizza'],['banana', 'pizza', 'fish', 'apple']];
 
+age = age.value ?? "";
+city = city.value ?? "";
 location = location.value ?? "";
-gender = gender.value ?? "";
-price = price.value ?? "";
-htype = htype.value ?? "";
+interest = interest.value ?? "";
+language = language.value ?? "":
+week = week.value ?? "";
+all = all.value ?? "";
 
+age_arr = age.split(",");
+city_arr = city.split(",");
 loc_arr = location.split(",");
-gen_arr = gender.split(",");
-prc_arr = price.split(",");
-htype_arr = htype.split(",");
+int_arr = interest.split(",");
+lan_arr = language.split(",");
+week_arr = week.split(",");
+all_arr = all.split(",");
   
-var arrays = [loc_arr,gen_arr,prc_arr,htype_arr];
+var arrays = [age_arr,city_arr,loc_arr,int_arr,lan_arr,week_arr,all_arr];
   
 var res1 = arrays.shift().filter(function(v) {
     return arrays.every(function(a) {
