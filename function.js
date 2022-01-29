ca1083a6327a3d458920c8a8fe5acab05107c725
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2022-01-29 16:22:22 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-window.function = function (age, city, location, interest, language, week, all) {
+window.function = function (age, city, location, interest, language, week) {
 
 //var arrays = [['apple', 'orange', 'banana', 'pear', 'fish', 'pancake', 'taco', 'pizza'],['taco', 'fish', 'apple', 'pizza'],['banana', 'pizza', 'fish', 'apple']];
 
@@ -8,17 +8,15 @@ location = location.value ?? "";
 interest = interest.value ?? "";
 language = language.value ?? "":
 week = week.value ?? "";
-all = all.value ?? "";
-
+  
 age_arr = age.split(",");
 city_arr = city.split(",");
 loc_arr = location.split(",");
 int_arr = interest.split(",");
 lan_arr = language.split(",");
 week_arr = week.split(",");
-all_arr = all.split(",");
-  
-var arrays = [age_arr,city_arr,loc_arr,int_arr,lan_arr,week_arr,all_arr];
+
+var arrays = [age_arr,city_arr,loc_arr,int_arr,lan_arr,week_arr];
   
 var res1 = arrays.shift().filter(function(v) {
     return arrays.every(function(a) {
@@ -28,6 +26,6 @@ var res1 = arrays.shift().filter(function(v) {
 
 var res2 = res1.join(', ');
 
-return res2
+return res2;
 
 }
